21d7709b243009391003abcf2f37cccb4605e012
Alex Kharichev <myx@myx.nz>
2026-09-25 18:45:52 +0300

    * DistroAgentTools

--- sh-scripts/DistroAgentsTools.fn.sh
+++ sh-scripts/DistroAgentsTools.fn.sh
@@ -401,6 +401,7 @@ case "$0" in
 
 		. "${MDLT_ORIGIN:=$MMDAPP/.local}/myx/myx.distro-agents/sh-lib/AgentsContext.include"
 		DistroAgentsContext --run-from-detect
+		DistroAgentsContext --distro-path-auto
 
 		## Copied from DistroLocalTools.fn.sh: needed for catMarkdown and the JSON escaper.
 		if   [ -d "$MYXROOT" ] && [ -f "$MYXROOT/bin/lib/catMarkdown.Common" ]; then
